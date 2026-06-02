2d2103d8d2711edb911f582bcd1a87cc19a0c56b
Cookieh-dev <okychampion@gmail.com>
2026-06-01 22:51:19 -0300

    Game structure -> needs polishing

--- PokerGame.c
+++ PokerGame.c
@@ -23,8 +23,26 @@ struct Deck {
 };
 
 struct Player {
-  char name[50];
+  int id;
+  int chips;
+  int numCardsInHand;
+  int hasFolded;
+  int hasBet;
   struct Card hand[5];
+};
+
+struct Dealer {
+  struct Card hand[5];
+  int numCardsInHand;
+};
+
+struct Game {
+  struct Player players[4];
+  struct Dealer dealer;
+  int numPlayers;
+  int currentPlayer;
+  int pot;
+  int minBet;
 };
 
 static const char *faceNames[] = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
@@ -33,42 +51,179 @@ static const char *rankNames[] = {"High Card", "One Pair", "Two Pair", "Three of
 
 struct Deck deck;
 
-struct Card dealCard();
+struct Card getCard();
 struct Rank getHandRank(struct Card hand[], int numCards);
+struct Rank evaluatePlayerHand(const struct Player *player, const struct Dealer *dealer);
 
 int main() {
   srand(time(NULL));
 
-  // generate a hand of 5 cards then check its rank
-  struct Card testHand[5];
-  for (int i = 0; i < 5; i++) {
-    testHand[i] = dealCard();
+  struct Game game;
+  printf("Insert the number of players (2-4): ");
+  if (scanf("%d", &game.numPlayers) != 1 || game.numPlayers < 2 || game.numPlayers > 4) {
+    printf("Invalid number of players. Please enter a number between 2 and 4.\n");
+    return 1;
   }
-  struct Rank handRank = getHandRank(testHand, 5);
-  printf("Hand Rank: %s\n", rankNames[handRank.rankValue - 1]);
-  printf(BLACK "Cards in Hand:\n");
-  for (int i = 0; i < 5; i++) {
-    // highlight repeated cards in red
-    int isRepeated = 0;
-    for (int j = 0; j < 5; j++) {
-      if (i != j && testHand[i].face == testHand[j].face && testHand[i].suit == testHand[j].suit) {
-        isRepeated = 1;
+  game.currentPlayer = 0;
+  game.pot = 0;
+  game.minBet = 10;
+  
+  // game is separated in rounds and turns, rounds reset the dealed cards and turns are the actions of the players, each player can bet, fold or check
+  // the game continues until only one player is left or all players have bet the same amount
+  int round = 1;
+
+  while (1) { // Rounds
+    struct Deck newDeck = {{0}, 0};
+    deck = newDeck; // reset the deck for each round
+    struct Dealer dealer;
+    dealer.numCardsInHand = 3;
+    for (int i = 0; i < dealer.numCardsInHand; i++) {
+      dealer.hand[i] = getCard();
+    }
+
+    for (int i = 0; i < game.numPlayers; i++) {
+      game.players[i].id = i + 1;
+      game.players[i].chips = 20; // starting chips
+      for (int j = 0; j < 2; j++)
+        game.players[i].hand[j] = getCard();
+      game.players[i].numCardsInHand = 2;
+      game.players[i].hasFolded = 0;
+      game.players[i].hasBet = 0;
+    }
+
+    int turn = 1;
+    
+    while (1) { // Turn
+      struct Player *currentPlayer = &game.players[game.currentPlayer];
+      // check if one player is left, if so end the round
+      int activePlayers = 0;
+      for (int i = 0; i < game.numPlayers; i++) {
+        if (!game.players[i].hasFolded) {
+          activePlayers++;
+        }
+      }
+      if (activePlayers == 1) {
+        currentPlayer->chips += game.pot;
         break;
       }
+      // check if the current player has folded, if so skip their turn
+      if (currentPlayer->hasFolded) {
+        game.currentPlayer++; // Move to the next player's turn
+        if (game.currentPlayer >= game.numPlayers) {
+          game.currentPlayer = 0;
+          turn++;
+        }
+        continue;
+      }
+
+      system("clear"); // Clear the console fto prevent cheating by looking at the previous player's hand
+
+      if (turn > 4) { // After the fourth turn, end the round
+        // determine the winner and distribute the pot
+        int bestRankValue = 0;
+        int bestRankFace = 0;
+        int bestRankSuit = 0;
+        int winnerId = 0;
+
+        for (int i = 0; i < game.numPlayers; i++) {
+          if (!game.players[i].hasFolded) {
+            struct Rank playerRank = evaluatePlayerHand(&game.players[i], &dealer);
+            if (playerRank.rankValue > bestRankValue || 
+                (playerRank.rankValue == bestRankValue && playerRank.rankFace > bestRankFace) || 
+                (playerRank.rankValue == bestRankValue && playerRank.rankFace == bestRankFace && playerRank.rankSuit > bestRankSuit)) {
+              bestRankValue = playerRank.rankValue;
+              bestRankFace = playerRank.rankFace;
+              bestRankSuit = playerRank.rankSuit;
+              winnerId = game.players[i].id;
+            }
+            printf("> Player %d has a %s\n", game.players[i].id, rankNames[playerRank.rankValue - 1]);
+          }
+        }
+        printf("\n");
+        printf("Player %d wins the round with a %s!\n \n", winnerId, rankNames[bestRankValue - 1]);
+        for (int i = 0; i < game.numPlayers; i++) {
+          if (game.players[i].id == winnerId) {
+            game.players[i].chips += game.pot;
+          }
+        }
+        break;
+      }
+
+      printf("[Turn %d] Player %d's turn. Chips: %d\n", turn, currentPlayer->id, currentPlayer->chips);
+      printf("Dealer's hand:\n");
+      for (int i = 0; i < dealer.numCardsInHand; i++) {
+        if (dealer.hand[i].suit == 0 || dealer.hand[i].suit == 1) {
+          printf(RED);
+        } else {
+          printf(BLACK);
+        }
+        printf("  %s of %s\n", faceNames[dealer.hand[i].face], suitNames[dealer.hand[i].suit]);
+        printf(RESET);
+      }
+
+      printf("Your hand (%s):\n", rankNames[evaluatePlayerHand(currentPlayer, &dealer).rankValue - 1]);
+      for (int i = 0; i < currentPlayer->numCardsInHand; i++) {
+        if (currentPlayer->hand[i].suit == 0 || currentPlayer->hand[i].suit == 1) {
+          printf(RED);
+        } else {
+          printf(BLACK);
+        }
+        printf("  %s of %s\n", faceNames[currentPlayer->hand[i].face], suitNames[currentPlayer->hand[i].suit]);
+        printf(RESET);
+      }
+      if (turn == 1) {
+        printf("Choose an action: 1) Bet 2) Fold 3) Check\n");
+      } else {
+        printf("Choose an action: 1) Raise 2) Fold 3) Check\n");
+      }
+      
+      int action;
+      if (scanf("%d", &action) != 1 || action < 1 || action > 3) {
+        printf("Invalid action. Please enter 1, 2, or 3.\n");
+        continue;
+      }
+
+      if (action == 2) { // Fold
+        currentPlayer->hasFolded = 1;
+        system("clear");
+        printf("Player %d folds.\n", currentPlayer->id);
+      } 
+
+      game.currentPlayer++; // Move to the next player's turn
+      
+      if (game.currentPlayer >= game.numPlayers) {
+        game.currentPlayer = 0;
+        turn++;
+
+        if (dealer.numCardsInHand < 5) {
+          dealer.hand[dealer.numCardsInHand] = getCard();
+          dealer.numCardsInHand++;
+        }
+        for (int i = 0; i < game.numPlayers; i++) {
+          if (game.players[i].numCardsInHand < 5) {
+            game.players[i].hand[game.players[i].numCardsInHand] = getCard();
+            game.players[i].numCardsInHand++;
+          }
+        }
+      }
     }
-    if (isRepeated) {
-      printf(RED);
+    turn = 1; // reset the turn counter for the next round
+    printf("Round %d ended. Pot: %d\n", round , game.pot);
+    printf("Do you want to play another round? (1 for Yes, 0 for No): ");
+    int playAgain;
+    if (scanf("%d", &playAgain) != 1 || playAgain < 0 || playAgain > 1) {
+      printf("Invalid input. Please enter 1 for Yes or 0 for No.\n");
+      return 1;
     }
-    else {
-      printf(BLACK);
+    if (playAgain == 0) {
+      break;
     }
-    
-    printf("%s of %s\n", faceNames[testHand[i].face], suitNames[testHand[i].suit]);
   }
+
   return 0;
 }
 
-struct Card dealCard() {
+struct Card getCard() {
   int i;
   int isUnique;
   struct Card cardToDeal;
@@ -105,7 +260,7 @@ struct Rank getHandRank(struct Card hand[], int numCards) {
   int flushSuit = 0;
 
   for (int i = 0; i < 4; i++) {
-    if (handSuitCounts[i] == 5) {
+    if (handSuitCounts[i] >= 5) {
       isFlush = 1; // indicates that it's a flush
       flushSuit = i; // store the suit of the flush
       break;
@@ -216,4 +371,15 @@ struct Rank getHandRank(struct Card hand[], int numCards) {
   }
 
   return rank;
+}
+
+struct Rank evaluatePlayerHand(const struct Player *player, const struct Dealer *dealer) {
+  struct Card combinedHand[player->numCardsInHand + dealer->numCardsInHand];
+  for (int i = 0; i < player->numCardsInHand; i++) {
+    combinedHand[i] = player->hand[i];
+  }
+  for (int i = 0; i < dealer->numCardsInHand; i++) {
+    combinedHand[player->numCardsInHand + i] = dealer->hand[i];
+  }
+  return getHandRank(combinedHand, player->numCardsInHand + dealer->numCardsInHand);
 }
